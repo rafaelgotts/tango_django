351ed18031550558f36a746d978d95656dba67a2
Rafael Gottsfritz <rafaelgotts@gmail.com>
2017-09-14 23:46:25 -0300

    Create first views for rango

--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+
+
+def index(request):
+    """ Create an intro pago for Rango, like
+        https://youtu.be/j7gp56w8T18?t=50s
+    """
+    response_body = (
+        'My name is...Rango<p>'
+        '<a href="/rango/about">Here</a> you can now more about me'
+    )
+    return HttpResponse(response_body)
+
+
+def about(request):
+    response_body = (
+        'Rango Says: Here is the about page.</p>'
+        '<a href="/rango/">Here</a> you can go back do index page.'
+    )
+    return HttpResponse(response_body)
